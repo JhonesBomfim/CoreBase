0ddf90920296538b7260bb852f7816905baeb11c
JhonesBomfim <jymbomfim@hotmail.com>
2025-12-11 20:10:14 -0300

    FEAT: Database file example

---
+++ Models/DataBaseExample.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreBase.Models
+{
+    public class Manager
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(512)]
+        public string? Usename { get; set; }
+
+        [StringLength(56)]
+        public string? Password { get; set; }
+
+    }
+}
