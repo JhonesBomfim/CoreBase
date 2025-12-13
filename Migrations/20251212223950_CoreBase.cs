806f4372d7a995ba5804ab92c3eafd6037e4b23e
Jhones Bomfim <103959700+JhonesBomfim@users.noreply.github.com>
2025-12-13 09:13:26 -0300

    Maintanence (#10)
    * Develop (#9)
    
    * INITIAL COMMIT (#1)
    
    * FEAT: Initial readme (#2)
    
    * Feat/dependencies (#3)
    
    * FEAT: Initial uv
    
    * FEAT: Pre-commit installed
    
    * FEAT: pre-commit configured
    
    * FEAT: Initial pipeline (#5)
    
    * FEAT: Initial pipeline
    
    * FEAT: Check Conflicts
    
    * FEAT: Check Draft
    
    * FEAT: Lint Initial
    
    * CHORE: needs for stage lint
    
    * FEAT: Check Documentation stage
    
    * FEAT: Initial Security
    
    * FEAT: Template Validate Initial
    
    * FEAT: Tag Release
    
    * FEAT: Version File
    
    * Feat/dependencies (#7)
    
    * FEAT: Initial uv
    
    * FEAT: Pre-commit installed
    
    * FEAT: pre-commit configured
    
    * FEAT: Framework and Pomelo installed
    
    * FEAT: Criptograph file (#6)
    
    * FEAT: Criptograph file
    
    * FEAT: Database file example
    
    * FEAT: Connection code into server file
    
    * FEAT: Services File to RestApi
    
    * FEAT: Controller code Initial (#8)
    
    * FEAT: Update NET version 8.0
    
    * CHORE: Update Number Version

---
+++ Migrations/20251212223950_CoreBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class CoreBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "varchar(512)", maxLength: 512, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "varchar(56)", maxLength: 56, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
